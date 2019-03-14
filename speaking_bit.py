e8b9a41716bd7c26652a87a0c30754f60d90d155
manawesome326 <manawesome326@gmail.com>
2019-03-14 11:42:49 +1100

    e

--- speaking_bit.py
+++ speaking_bit.py
@@ -42,7 +42,7 @@ def are_you_death_funct():
 	print2("After all, they were holding a scythe...\n", 2)
 	print2("𝒀𝒆𝒔", 2)
 	print2("You react to this by freaking out.",2)
-	print2('"Hah! I\'m just messing with you."', 2)
+	print2('"Hah! I\'m just messing with you. My name\'s Jimmy"', 2)
 	print2("You stop freaking out, and go back to the base level of 'kinda freaked out' that you've been in all this time.", 3.1)
 	print2('"No, I\'m not him, I just work for him, and he prefers the name \'The Grim Reaper\', or just \'Grim\' if you\'re cool, like me."', 3.6)
 	print2('"...I\'m still working on my impression"')
@@ -91,6 +91,17 @@ def whys_it_walmart_funct():
 	datetime_nonsense = (datetime(1,1,1) + timedelta(seconds=(time.time() -digest["entry time"])))
 	print2('"Don\'t ask me. It didn\'t look like one ' + str(datetime_nonsense.minute) + ' minutes and ' + str(datetime_nonsense.second) + ' seconds ago."', 3)
 
+def how_to_leave_funct():
+	print2("You're getting sick of all this dialogue, aren't you? Nevermind all the effort I put into it. Fine, just skip to the cool battle scene.\n", 4)
+	print2('"Well, funny story actually..."', 2)
+	print2('"I haven\'t really got a clue. It\'s only my third day on the job, Grim never told me what to do when this happened, so you may as well just settle--"',4)
+	print2("You hear the sound of a phone ringing.", 2)
+	print2("*ʀɪɴɢ... ʀɪɴɢ...*", 2)
+	print2("Jimmy digs a phone out from his cloak, and answers.", 2)
+	print2("The ensuing conversation didn't make a lot of sense to you.", 2)
+	print2('"Well that was good timing. Grim\'s got a job for you. Says that it\'s "very self-explanatory". Any second now you should--"', 4)
+	print2("And then everything goes dark.")
+
 are_you_death = Question("Death?", are_you_death_funct)
 where_am_i = Question("Where am I?", where_am_i_funct)
 how_to_pay = Question("How do I pay for all this stuff I'm carrying?", how_to_pay_funct)
@@ -98,6 +109,8 @@ ate_loose_pretzels = Question("What was up with those loose pretzels earlier?", 
 whats_death_like = Question("What's working for the Grim Reaper like?", whats_death_like_funct)
 scouted_an_aisle = Question("What was up with the 'scouted an aisle for 50 points' thing from earlier?", scouted_an_aisle_funct)
 whys_it_walmart = Question("Why does purgatory look like a department store?", whys_it_walmart_funct)
+
+how_to_leave = Question("So how do I get out of here?", how_to_leave_funct)
 
 free_options = []
 available_options = [are_you_death, where_am_i]
@@ -116,9 +129,15 @@ while True:
 	choice = make_a_choice(available_options)
 	time.sleep(0.1)
 	choice.response()
+	if choice == how_to_leave:
+		break
 	print2("")
 	try:
 		available_options.append(free_options.pop(0))
 	except IndexError:
 		pass
 	turns += 1
+	if turns - turns_at_dialog_start == 6:
+		available_options.append(how_to_leave)
+
+darkness_envelops()
